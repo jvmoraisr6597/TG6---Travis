061d9c00c4d4f29fc4a8e79037b39368c141994f
Caik de Moraes Souza <caikdemoraes@gmail.com>
2021-09-25 20:15:08 -0300

    travis

--- src/test/java/com/mackenzie/tg6/TestTG6.java
+++ src/test/java/com/mackenzie/tg6/TestTG6.java
@@ -1,25 +1,68 @@
 package com.mackenzie.tg6;
 
-import com.mackenzie.tg6.models.Cliente;
 import static org.junit.Assert.assertEquals;
 
+import com.mackenzie.tg6.models.Cliente;
 
 import org.junit.Test;
 
 public class TestTG6 {
     @Test
-    public void testeEmprestimo() {
-        String liberado = "Empréstimo Concedido";
+    public void CT1() {
         String negado = "Empréstimo Negado";
-        System.out.println("Teste Empréstimo");
-        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(2000, true, true)));
-        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(2000, false, true)));
-        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(2000, true, false)));
-        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(2000, false, false)));
 
-        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(950, false, true)));
-        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(950, true, false)));
-        assertEquals(negado, TG6.liberarEmprestimo(new Cliente(950, true, true)));
         assertEquals(negado, TG6.liberarEmprestimo(new Cliente(950, false, false)));
+    }
+
+    @Test
+    public void CT2(){
+        String liberado = "Empréstimo Concedido";
+        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(2000, true, true)));
+    }
+
+    @Test
+    public void CT3(){
+        String liberado = "Empréstimo Concedido";
+        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(2000, true, true)));
+    }
+    @Test
+    public void CT4(){
+        String liberado = "Empréstimo Concedido";
+        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(2000, false, true)));
+
+    }
+    @Test
+    public void CT5(){
+        String liberado = "Empréstimo Concedido";
+        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(2000, true, false)));
+
+    }
+    @Test
+    public void CT6(){
+        String liberado = "Empréstimo Concedido";
+        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(2000, false, false)));
+    }
+    @Test
+    public void CT7(){
+        String liberado = "Empréstimo Concedido";
+        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(950, false, true)));
+
+    }
+    @Test
+    public void CT8(){
+        String liberado = "Empréstimo Concedido";
+        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(950, true, false)));
+
+    }
+    @Test
+    public void CT9(){
+        String liberado = "Empréstimo Concedido";
+        assertEquals(liberado, TG6.liberarEmprestimo(new Cliente(950, true, false)));
+    }
+    @Test
+    public void CT10(){
+        String negado = "Empréstimo Negado";
+        assertEquals(negado, TG6.liberarEmprestimo(new Cliente(950, true, true)));
+
     }
 }
